e6d126a84f6233e7bcc8db94160ba3649a2a3dc1
Kiswono Prayogo <kiswono@gmail.com>
2022-07-09 00:32:37 +0700

    add more fts to check

---
+++ 2022fts/postgres_gin_trgm/gin_trgm_ops.go
@@ -0,0 +1,9 @@
+package main
+
+// TODO: pgxpool
+// CREATE INDEX index_name ON table_name USING gin(column_name gin_trgm_ops);
+// WHERE column_name ILIKE '%search_term%';
+
+func main() {
+
+}
---
+++ 2022fts/solr/solr.go
@@ -0,0 +1,8 @@
+package main
+
+// TODO: vanng822/go-solr or mysql
+// WHERE MATCH(column_name) AGAINST('search_term' IN BOOLEAN MODE);
+
+func main() {
+
+}
