56381f833e8049d017cfeee8b52bd4a346b529d4
Kiswono Prayogo <kiswono@gmail.com>
2022-07-09 00:34:14 +0700

    add two more fts

---
+++ 2022fts/mariadb_aria_fulltext/fulltext.go
@@ -0,0 +1,9 @@
+package main
+
+// TODO: go-sql-driver/mysql
+// CREATE FULLTEXT INDEX index_name ON table_name(column_name);
+// WHERE MATCH(column_name) AGAINST('search_term' IN BOOLEAN MODE);
+
+func main() {
+
+}
